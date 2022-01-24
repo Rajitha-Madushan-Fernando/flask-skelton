fb9dc3fdfe459cbab37b14762ff5ec0287b6176f
Rajitha-Madushan-Fernando <buddikarajitha@yahoo.com>
2022-01-24 19:27:24 +0100

    change python path

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -4,6 +4,8 @@ from flask import Blueprint, flash, g, redirect, render_template, request, sessi
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
+#Add comment
+
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
